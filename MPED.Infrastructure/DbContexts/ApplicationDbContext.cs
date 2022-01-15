434c15802561dbf09648417161089c55ce0edd73
smehrzad <smehrzad@crema.com.tr>
2022-01-15 16:22:21 +0300

    Add Contexts Add Result model for API response Add Identity result models

---
+++ MPED.Infrastructure/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MPED.Infrastructure.DbContexts
+{
+    public class ApplicationDbContext : IApplicationDbContext
+    {
+        public IDbConnection Connection => throw new NotImplementedException();
+
+        public bool HasChanges => throw new NotImplementedException();
+
+        public EntityEntry Entry(object entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> SaveChangesAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
