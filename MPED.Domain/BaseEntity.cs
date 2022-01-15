58ad119a96b068556a393b772de173c914b17970
smehrzad <smehrzad@crema.com.tr>
2022-01-15 12:52:01 +0300

    Add essential layers

---
+++ MPED.Domain/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPED.Domain
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public string CreatedBy { get; set; }
+
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+
+        public string LastModifiedBy { get; set; }
+
+        public DateTime? LastModifiedOn { get; set; }
+    }
+}
